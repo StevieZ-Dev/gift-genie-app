cb4577b0450fb7efda1825a9f13afa6f54277380
Stevie Z <steviezillosophy@gmail.com>
2025-12-10 21:54:09 -0500

    Fix missing CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,14 +11,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', minHeight: '100vh' }}>
-      {/* GOOGLE ADSENSE SCRIPT */}
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6068418321673019"
-        crossOrigin="anonymous"
-      />
-      {children}
-    </div>
+    <html lang="en">
+      <head>
+        {/* GOOGLE ADSENSE SCRIPT */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6068418321673019"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body style={{ fontFamily: 'system-ui, sans-serif' }}>{children}</body>
+    </html>
   );
 }
